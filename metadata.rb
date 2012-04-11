87d0eca80e34eed9f7d81b3ed5b729cbef3677da
Shaun Dern <shaun@substantial.com>
2012-04-10 18:00:53 -0700

    wasn't gettign package

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "shaun@substantial.com"
 license          "All rights reserved"
 description      "Installs/Configures firefox"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.3"
+version          "0.0.4"
--- recipes/install_other_versions.rb
+++ recipes/install_other_versions.rb
@@ -6,7 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-if node.kernel.machine == "x86_84"
+if node.kernel.machine != "x86"
   package "ia32-libs"
 end
 
